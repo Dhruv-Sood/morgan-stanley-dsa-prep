f8a3bd019431111056adc30735b3b4efa52c271d
Dhruv Sood <dhruvsoodboss@gmail.com>
2025-01-04 21:30:01 +0530

    Palindrome String

---
+++ palindrome_string.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    // Function to check if a string is a palindrome.
+    bool isPalindrome(string &s)
+    {
+        // code here
+        int i = 0;
+        int j = s.size() - 1;
+        while (i <= j)
+        {
+            if (s[i] != s[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
